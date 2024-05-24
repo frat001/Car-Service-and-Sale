3d0341d4517280a167d2a9656f17f65014185ecb
fırat pıçak <firatozgurp@gmail.com>
2024-05-25 00:25:01 +0300

    Login

--- OtoServisSatis.Entities/Kullanici.cs
+++ OtoServisSatis.Entities/Kullanici.cs
@@ -12,10 +12,10 @@ namespace OtoServisSatis.Entities
     {
         public int Id { get; set; }
 
-        [StringLength(50), Display(Name = "Adı"), Required(ErrorMessage = "{0} Alan Zorunludur")]
+        [StringLength(50), Display(Name = "Ad"), Required(ErrorMessage = "{0} Alan Zorunludur")]
         public string Adi { get; set; }
 
-        [StringLength(50), Display(Name = "Soyadı"), Required(ErrorMessage = "{0} Alan Zorunludur")]
+        [StringLength(50), Display(Name = "Soyad"), Required(ErrorMessage = "{0} Alan Zorunludur")]
         public string Soyadi { get; set; }
 
         [StringLength(50), Required(ErrorMessage = "{0} Alan Zorunludur")]
--- OtoServisSatis.Entities/Musteri.cs
+++ OtoServisSatis.Entities/Musteri.cs
@@ -19,11 +19,11 @@ namespace OtoServisSatis.Entities
         public string Soyadi { get; set; }
         [StringLength(11), Display(Name = "TC Kimlik No")]
         public string? TcNo { get; set; }
-        [StringLength(50)]
+        [StringLength(50), Required(ErrorMessage = "{0} Alan Zorunludur")]
         public string Email { get; set; }
         [StringLength(500)]
         public string? Adres { get; set; }
-        [StringLength(20)]
+        [StringLength(20), Required(ErrorMessage = "{0} Alan Zorunludur")]
         public string Telefon { get; set; }
         public string? Notlar { get; set; }
         [Display(Name = "Araç")]
